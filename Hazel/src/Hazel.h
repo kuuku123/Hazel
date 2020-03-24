b55250f4fbbc451205f79bcfd97724fd46be62c8
kuuku1234 <kuuku123@yonsei.ac.kr>
2020-03-24 13:36:27 +0900

    fixing git submodule things

--- Hazel/src/Hazel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use by Hazel application
-#include <stdio.h>
-#include "Hazel\Application.h"
-
-//------------Entry Point-----------------
-#include "Hazel\EntryPoint.h"
-//----------------------------------------
